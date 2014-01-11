2679fdd427394d4b36239b5c62d89348c3b0a220
Ahmad Abdel Naser <dev.ahmadnaser@gmail.com>
2014-01-11 21:14:09 +0200

    mvc5 tricks series
    mvc5 tricks series

---
+++ MVC5Tricks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5Tricks.MvcApplication" Language="C#" %>
---
+++ MVC5Tricks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Tricks.Startup))]
+namespace MVC5Tricks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
